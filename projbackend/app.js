e6a4bec622daaad79714bfaade29bea969df0670
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2020-10-25 23:26:52 -0700

    Update app.js
    added middlewares

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const { Mongoose } = require("mongoose");
-
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { cookie } = require('express-validator');
 const port = process.env.PORT || 8000;
 app.get('/', (req, res) => {
 return 	res.send("Welcome to Home Page!");
@@ -12,6 +13,9 @@ return 	res.send("Welcome to Home Page!");
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 mongoose.connect(
 process.env.DATABASE
 , {

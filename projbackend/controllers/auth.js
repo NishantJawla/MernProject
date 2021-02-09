d0b4e05f18833c371bdbc0d4ece68eaeb4a85a79
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-02-09 08:56:57 -0800

    Created a token and signin

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { check , validationResult} = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 exports.signup = (req,res) => {
     
     const errors = validationResult(req);
@@ -24,6 +26,39 @@ exports.signup = (req,res) => {
         });
     });
 }
+exports.signin = (req, res) => {
+    const {email,password} = req.body;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg,
+            parameter: errors.array()[0].param
+        })
+    }
+    User.findOne({email} , (err,user) => {
+        if(err) {
+            res.status(400).json({
+                error: "User Email does not exist"
+            })
+        }
+    })
+    if(!user.authenticate(password)){
+      return  res.status(401).json({
+            error: "Email and Password does not match"
+        })
+    }
+    //create token
+    const token = jwt.sign({_id: user._id}, process.env.SECRET);
+    //put token in cookie
+    res.cookie("token", token, {expire: new Date() + 9999});
+    //send response to frontend
+    const {_id, name, role} = user;
+    return res.json({
+        token,
+        user: {_id, name, email, role}
+    });
+}
+
 exports.signout = (req, res) => {
     res.json ({
      message: "user signout!"
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { check , validationResult} = require('express-validator');
-const {signout, signup} = require('../controllers/auth');
+const {signout, signup , signin} = require('../controllers/auth');
 router.get("/signout", signout);
 router.post('/signup',[
     check("name", "name should be atleast 3 characters").isLength({ min: 3 }),
     check("email" , "email is required").isEmail(),
     check("password", "password should be atleast 3 characters").isLength({ min: 3 })
 ], signup);
+router.post('/signin',[
+    check("email" , "email is required").isEmail(),
+    check("password", "password field is required").isLength({ min: 1 })
+], signin);
 module.exports = router;

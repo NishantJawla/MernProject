bd54be2c8a4b6e818912564fdbf0a3cb1824d0a8
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-04-11 17:18:02 +0530

    created getCategory and getallcategory Routes and middlewares

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -11,3 +11,30 @@ exports.getCategoryById = (req, res, next,id) => {
         next();
     })
 }
+
+exports.createCategory = (req, res) => {
+    const category = new Category(req.body);
+    category.save().then((err, category) => {
+        if(err){
+            return res.status(400).json({
+                errors: "Not able to save category in DB"
+            })
+        }
+        res.json({category})
+    });
+}
+
+exports.getCategory = (req, res) => {
+    return res.json(req.category);
+}
+
+exports.getAllCategory = (req, res) => {
+    Category.find().exec((err, category) => {
+    if(err){
+        return res.status(400).json({
+            errors: "No category found"
+        })
+    }
+    return res.json(category);
+    })
+}
--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {getCategoryById} = require('../controllers/category');
+const {getCategoryById,createCategory,getAllCategory,getCategory} = require('../controllers/category');
 const {isSignedIn,isAuthenticated,isAdmin} = require('../controllers/auth');
 const {getUserById} = require('../controllers/user');
 router.param("userId",getUserById);
 router.param("categoryId",getCategoryById);
+//create
+router.post("/category/create/:userId",isSignedIn,isAuthenticated,isAdmin,createCategory);
+//read
+router.get("/category/:categoryId",getCategory);
+router.get("/categories",getAllCategory);
+//update
+//delete
 module.exports = router;

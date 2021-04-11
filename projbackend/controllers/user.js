0a65a1d6eef2511f6a3f88ec18d554060ec4630b
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-04-11 16:16:34 +0530

    Created push order in purchase list

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -49,3 +49,28 @@ exports.listOrderList = (req, res) => {
     })
     
 }
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+    let purchases = []
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        });
+    });
+    User.findOneAndUpdate({_id: req.profile.id},{$push: {purchases: purchases}},
+        {new: true, useFindAndModify: false},(err, purchases) => {
+            if(err){
+            return res.status(400).json({
+                error: "Unable to update purchase list"
+            })
+            }
+            next();
+            //otherwise hand over the control to the next
+        })
+}

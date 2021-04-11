1bd2dcb9ee0f34527d638638a7a4d48fd1c62e65
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-04-11 23:16:42 +0530

    created product route and controller
    That can accept form data

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require('../models/Product');
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
 exports.getProductById = (req, res, next, id) => {
     Product.findById(id).exec((err, product) =>{
         if(err){
@@ -9,5 +12,35 @@ exports.getProductById = (req, res, next, id) => {
         }
         req.product = product;
         next();
+    })
+}
+exports.createProduct = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req,(err,fields, file) => {
+        if(err) {
+            return res.status(400).json({
+                errors: "Photo failed to save"
+            })
+        }
+        //TODO: validations on fields
+        let product = new Product(fields);
+        if(file.photo){
+            if(file.photo.size> 3000000){
+                return res.status(400).json({
+                    errors: "Photo size must below 3000000"
+                })
+            }
+            product.photo.data = fs.appendFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+        product.save((err, product)=>{
+            if(err){
+                return res.status(400).json({
+                    errors: "failed to save product"
+                })
+            }
+            res.json(product)
+        })
     })
 }
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getProductById} = require('../controllers/product');
+const {getProductById, createProduct} = require('../controllers/product');
 const {isAdmin,isSignedIn,isAuthenticated} = require('../controllers/auth')
 const {getUserById} = require('../controllers/user');
+//all my params
 router.param("userId",getUserById);
 router.param("productId",getProductById);
+//all my routers
+router.post("/products/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct);
 module.exports = router;

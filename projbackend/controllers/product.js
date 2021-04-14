2b2ff016b003e75b1027bcb2f6498cdcc66dcebf
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-04-14 14:57:27 +0530

    Added create product validations

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -15,32 +15,43 @@ exports.getProductById = (req, res, next, id) => {
     })
 }
 exports.createProduct = (req, res) => {
-    let form = new formidable.IncomingForm();
+    let form = formidable.IncomingForm();
     form.keepExtensions = true;
-    form.parse(req,(err,fields, file) => {
-        if(err) {
-            return res.status(400).json({
-                errors: "Photo failed to save"
-            })
+
+    form.parse(req, (err, fields, file) => {
+    if (err) {
+        return res.status(400).json({
+        error: "problem with image",
+        });
+    }
+
+      //destructure the fields
+    const { name, price, description, category, stock } = fields;
+    if (!name || !description || !price || !category || !stock) {
+        return res.status(400).json({
+        error: "please include all fields",
+        });
+    }
+
+    let product = new Product(fields);
+      //handle file here
+    if (file.photo) {
+        if (file.photo.size > 3000000) {
+        return res.status(400).json({
+            error: "File size too big",
+        });
         }
-        //TODO: validations on fields
-        let product = new Product(fields);
-        if(file.photo){
-            if(file.photo.size> 3000000){
-                return res.status(400).json({
-                    errors: "Photo size must below 3000000"
-                })
-            }
-            product.photo.data = fs.appendFileSync(file.photo.path);
-            product.photo.contentType = file.photo.type;
+        product.photo.data = fs.readFileSync(file.photo.path);
+        product.photo.contentType = file.photo.type;
+    }
+      // save to the db
+    product.save((err, product) => {
+        if (err) {
+        res.status(400).json({
+            error: "saving tshirt in db failed",
+    });
         }
-        product.save((err, product)=>{
-            if(err){
-                return res.status(400).json({
-                    errors: "failed to save product"
-                })
-            }
-            res.json(product)
-        })
-    })
-}
+        res.json(product);
+    });
+    });
+};

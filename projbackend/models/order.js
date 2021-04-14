b58683fb5f5d148186571714dcc619260f309024
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-04-14 18:30:02 +0530

    created order routes and controllers

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -20,6 +20,11 @@ const OrderSchema = new mongoose.Schema(
     amount: { type: Number },
     address: String,
     updated: Date,
+    status: {
+      type:String,
+      default: "Received",
+      enum: ["canceled","Delivered","Shipped","Processing","Received"]
+    },
     user: {
       type: ObjectId,
       ref: "User"

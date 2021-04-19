c552b4ec06b1b76e65d98a230bd47bfb778dccf7
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-04-19 21:52:56 +0530

    Created a base layout in react

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import "../styles.css";
+import {API} from "../backend"
 export default function Home() {
+  console.log("API is available at",process.env.REACT_APP_BACKEND)
   return (
     <div>
-      <h1>Hello front end</h1>
+      <h1 className="text-white">Hello front end</h1>
     </div>
   );
 }
